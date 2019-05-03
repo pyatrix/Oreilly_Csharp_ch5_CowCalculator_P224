3eeb2b9890fc0979527b5bf67d6e982983861748
Peng <Peng@PENG-PC>
2019-05-03 11:27:18 +0800

    P224  finish

--- Oreilly_Csharp_ch5_CowCalculator_P224/Form1.Designer.cs
+++ Oreilly_Csharp_ch5_CowCalculator_P224/Form1.Designer.cs
@@ -28,20 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Calculate_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.numericUpDown1.Location = new System.Drawing.Point(73, 17);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 27);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // Calculate_button
+            // 
+            this.Calculate_button.Font = new System.Drawing.Font("新細明體", 12F);
+            this.Calculate_button.Location = new System.Drawing.Point(35, 66);
+            this.Calculate_button.Name = "Calculate_button";
+            this.Calculate_button.Size = new System.Drawing.Size(117, 28);
+            this.Calculate_button.TabIndex = 0;
+            this.Calculate_button.Text = "Calculate";
+            this.Calculate_button.UseVisualStyleBackColor = true;
+            this.Calculate_button.Click += new System.EventHandler(this.Calculate_button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 174);
+            this.ClientSize = new System.Drawing.Size(215, 149);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Calculate_button);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button Calculate_button;
     }
 }
 
--- Oreilly_Csharp_ch5_CowCalculator_P224/Form1.cs
+++ Oreilly_Csharp_ch5_CowCalculator_P224/Form1.cs
@@ -12,9 +12,21 @@ namespace Oreilly_Csharp_ch5_CowCalculator_P224
 {
     public partial class Form1 : Form
     {
+        Farmer farmer;
         public Form1()
         {
             InitializeComponent();
+            farmer = new Farmer() { NumberOfCows = 15 };
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender , EventArgs e)
+        {
+            farmer.NumberOfCows = (int)numericUpDown1.Value;
+        }
+
+        private void Calculate_button_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("I need {0} bags od feed for {1} cows", farmer.BagsOfFeed, farmer.NumberOfCows);
         }
     }
 }
