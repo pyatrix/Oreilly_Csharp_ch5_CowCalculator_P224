e966362ae148a926624ab0c0267bedc1491312b2
Peng <Peng@PENG-PC>
2019-05-03 09:50:05 +0800

    P222 finish

---
+++ Oreilly_Csharp_ch5_CowCalculator_P224/Farmer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreilly_Csharp_ch5_CowCalculator_P224
+{
+    class Farmer
+    {
+        public int BagsOfFeed;
+        public const int FeedMultiplier = 30;
+        public int GetNumberOfCows()
+        {
+            return numberOfCows;
+        }
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+    }
+}
